c68c53bee42fc06dcfdf92b67865c8a625afb1eb
PrathamD16 <pratham.doke.1602@gmail.com>
2023-12-22 00:08:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import SmartServer from "./Images/logo.jpg";
+import SuccesfullLogin from "./SuccesfullLogin";
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [login, isLoggedIn] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -35,64 +37,69 @@ const App = () => {
       emailPattern.test(username.trim()) &&
       passwordPattern.test(password.trim())
     ) {
-      console.log("Login successful!");
-      // Add your login logic or redirection here
+      //Successfulll login
+      isLoggedIn(true);
     }
   };
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-slate-900">
-      <div className="bg-black shadow-md rounded px-8 py-8 sm:px-12 sm:py-12 w-96">
-        <h2 className="text-2xl mb-6">
-          <img src={SmartServer} alt="" />
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <input
-              placeholder="email"
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
-              required
-            />
-            {emailError && (
-              <p className="text-red-500 text-xs mt-1 w-full h-5">
-                {emailError}
-              </p>
-            )}
+      <div className={!login ? `block` : `hidden`}>
+        <div className="bg-black shadow-md rounded px-8 py-8 sm:px-12 sm:py-12 w-96">
+          <h2 className="text-2xl mb-6">
+            <img src={SmartServer} alt="" />
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <input
+                placeholder="email"
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
+                required
+              />
+              {emailError && (
+                <p className="text-red-500 text-xs mt-1 w-full h-5">
+                  {emailError}
+                </p>
+              )}
+            </div>
+            <div className="mb-6">
+              <input
+                placeholder="password"
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
+                required
+              />
+              {passwordError && (
+                <p className="text-red-500 text-xs mt-1 w-full h-5">
+                  {passwordError}
+                </p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Login
+            </button>
+          </form>
+          <div className="flex justify-center text-gray-300 mt-3 text-sm">
+            <a href="" className="underline">
+              Forgot your password
+            </a>
           </div>
-          <div className="mb-6">
-            <input
-              placeholder="password"
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full border border-gray-300 rounded py-2 px-3 focus:outline-none focus:border-blue-500"
-              required
-            />
-            {passwordError && (
-              <p className="text-red-500 text-xs mt-1 w-full h-5">
-                {passwordError}
-              </p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Login
-          </button>
-        </form>
-        <div className="flex justify-center text-gray-300 mt-3 text-sm">
-          <a href="" className="underline">
-            Forgot your password
-          </a>
         </div>
+      </div>
+      <div className={login ? `block` : `hidden`}>
+        <SuccesfullLogin />
       </div>
     </div>
   );
---
+++ src/SuccesfullLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SuccesfullLogin() {
+  return (
+    <div className="text-white text-2xl bg-green-600 p-10 rounded-md">
+      <p>Login successfull!!</p>
+    </div>
+  );
+}
+
+export default SuccesfullLogin;
